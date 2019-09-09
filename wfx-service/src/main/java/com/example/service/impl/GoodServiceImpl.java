9120b477c50f660009bac07d7417f5b357ac0b84
ganwang <caiwen_@126.com>
2019-09-09 10:11:48 +0800

    完成使用 七牛服务器，实现商品信息的添加操作

--- wfx-service/src/main/java/com/example/service/impl/GoodServiceImpl.java
+++ wfx-service/src/main/java/com/example/service/impl/GoodServiceImpl.java
@@ -17,6 +17,7 @@ public class GoodServiceImpl implements GoodService {
 
     @Override
     public boolean saveGood(WxbGood wxbGood) {
+        // 代码生成主键（注意：使用UUID，需要将wxb_good表中的主键的 长度 修改为 varchar(60)
         wxbGood.setGoodId(UUID.randomUUID().toString());
         //添加的商品，状态默认为“待审核 0”
         wxbGood.setState(0L);
