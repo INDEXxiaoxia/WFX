572a8c0520c8a4c66a8cc20754e33ea86a5fe192
ganwang <caiwen_@126.com>
2019-09-10 11:45:36 +0800

    添加 demo案例代码

--- wfx-demo/src/main/java/com/example/demo/DemoController.java
+++ wfx-demo/src/main/java/com/example/demo/DemoController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.example.model.vo.Result;
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,40 +26,29 @@ public class DemoController {
 
     @RequestMapping("/fileUplad")
     @ResponseBody
-    public HashMap<String, String> fileUpload(HttpServletRequest request, HttpServletResponse response, MultipartFile imgFile) {
+    public HashMap fileUpload(HttpServletRequest request, HttpServletResponse response, MultipartFile imgFile) {
         /*
             1.在java中可以获取工程的两种路径：
                 服务器路径：当前服务器所部署的路径：request.getServletContext().getRealPath("/remote_image")
                 本地程序的路径 ：通过 File对象操作 new File(parentPath,fileName)
 
          */
-
-//        String path = request.getServletContext().getRealPath("/remote_image");
-
-        //保存返回的结果
-        JSONObject obj = new JSONObject();
-
-        //实现将imageFile保存到本地路径
+        //改造的目的：是希望将界面中传递过来的图片，直接保存到 /resources/static目录下。如果保存在静态Static目录，直接通过/文件名 访问
+        //下面的这个方法，可以获取springboot工程中，指定静态资源的绝对路径目录
+        String path = ClassUtils.getDefaultClassLoader().getResource("static").getPath();
+        //实现将imageFile保存到springboot静态资源目录下
         try {
             //1 获取的上传的文件的完整名称（包含后缀）
             String fileName = imgFile.getOriginalFilename();
-            //2 创建将文件保存到本地磁盘的文件目录
-            String parentPath = "d:\\wfx-images";
-            //3 优化：在保存图片到本地指定目录时，需要确保图片的名称唯一
-            String urlFileName = UUID.randomUUID().toString() + "." + fileName.substring(fileName.lastIndexOf("."));
-            File file = new File(parentPath, urlFileName);//创建一个对象
+            //2 生成一个文件的随机名称，防止重复
+            String urlFileName = UUID.randomUUID().toString() + fileName.substring(fileName.lastIndexOf("."));
+            File file = new File(path, urlFileName);//创建一个对象
             //4 编写自定义方法，将imageFile对象参数保存到指定目录中
             writeToLocal(file, imgFile.getInputStream());
             //5 将上传成功后的图片，返回给界面，进行显示
-
-//            //下面的代码，即为kindeditor官方案例中提供的返回文件上传成功后的 返回结果集的拼接
-//            PrintWriter out = new PrintWriter(response.getOutputStream());
-//            obj.put("error", 0);
-//            obj.put("url", "http://localhost:8080/001.jpg");
-//            out.println(obj.toJSONString());
             HashMap map = new HashMap();
             map.put("error", 0);
-            map.put("url", "http://localhost:8080/001.jpg");
+            map.put("url", "/" + urlFileName);
             return map;
         } catch (IOException e) {
             e.printStackTrace();
